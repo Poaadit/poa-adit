50cd0722906097c5e12daaa50884311adb8e9d77
Poaadit <105600040+Poaadit@users.noreply.github.com>
2024-08-12 04:25:52 +0100

    Update AbtSect2.jsx

--- src/Pages/About/AbtSect2.jsx
+++ src/Pages/About/AbtSect2.jsx
@@ -2,12 +2,11 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Button2, ColumnContainer } from "../../Components/ResponsiveConts";
 import "./About.scss";
-import { Modal1, Modal2, Modal4, Modal5 } from "./Modals"; const AbtSect2 = () => {
+import { Modal1, Modal2, Modal3, Modal4 } from "./Modals"; const AbtSect2 = () => {
   const [modal1Visible, setmodal1Visible] = useState(false);
   const [modal2Visible, setmodal2Visible] = useState(false);
-
+  const [modal3Visible, setmodal3Visible] = useState(false);
   const [modal4Visible, setmodal4Visible] = useState(false);
-  const [modal5Visible, setmodal5Visible] = useState(false);
 
   const Team = [
     {
@@ -22,18 +21,17 @@ import { Modal1, Modal2, Modal4, Modal5 } from "./Modals"; const AbtSect2 = () =
       paragraph:
         "He is an astute professional with over twenty-three (23) years cognate experience in the built environment. He holds a B-Tech and M-Tech in Architecture from the Federal Univeristy of Technology, Akure, Ondo State, Nigeria. He is a fellow of the Nigeria Institute of Architects (NIA) and a member of Architects Registration Council of Nigeria (ARCON). Bode is an International associate of American Institute of Architects (AIA) and also a fellow of the International professional Managers Association, Uk. He is an astute human resource manager, with focus on optimization, corporate transformation and corporate management.",
     },
-
     {
       name: "Dolapo Akinwale",
       rank: "Strategy & Business Development",
-      more: <h6 onClick={() => setmodal4Visible(true)}>Read More</h6>,
+      more: <h6 onClick={() => setmodal3Visible(true)}>Read More</h6>,
       paragraph:
         "Mr Akinwale holds a First Class Honors Bachelors degree in Computer Engineering from Convenant Univeristy, Nigeria. He further obtained a Masters Degree in Human Resource (Service) Management- University of Buckingham, United Kingdom. He is a member of the International Institute of Business Analysis, (IIBA). He worked in Onet Telecommunications located in Oyo State and Airtel Nigeria. He has experience in Human Resource Management, Service Marketing. and Organisational Strategy.",
     },
     {
       name: "Olaleye Adeyinka",
       rank: "Chief Financial Officer",
-      more: <h6 onClick={() => setmodal5Visible(true)}>Read More</h6>,
+      more: <h6 onClick={() => setmodal4Visible(true)}>Read More</h6>,
       paragraph:
         "Mr Akinwale holds a First Class Honors Bachelors degree in Computer Engineering from Convenant Univeristy, Nigeria. He further obtained a Masters Degree in Human Resource (Service) Management- University of Buckingham, United Kingdom. He is a member of the International Institute of Business Analysis, (IIBA). He worked in Onet Telecommunications located in Oyo State and Airtel Nigeria. He has experience in Human Resource Management, Service Marketing. and Organisational Strategy.",
     },
@@ -78,9 +76,8 @@ import { Modal1, Modal2, Modal4, Modal5 } from "./Modals"; const AbtSect2 = () =
           <>
             <Modal1 visible={modal1Visible} setVisible={setmodal1Visible} />
             <Modal2 visible={modal2Visible} setVisible={setmodal2Visible} />
-            
-            <Modal4 visible={modal4Visible} setVisible={setmodal4Visible} />
-            <Modal5 visible={modal5Visible} setVisible={setmodal5Visible} />
+            <Modal4 visible={modal3Visible} setVisible={setmodal3Visible} />
+            <Modal5 visible={modal4Visible} setVisible={setmodal4Visible} />
           </>
         </div>
       </ColumnContainer>

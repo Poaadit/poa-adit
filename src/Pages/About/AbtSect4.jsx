d155179bb640beed845fbb5edf7f85805e61715c
Poaadit <105600040+Poaadit@users.noreply.github.com>
2025-08-25 08:28:37 +0000

    Update AbtSect4.jsx

--- src/Pages/About/AbtSect4.jsx
+++ src/Pages/About/AbtSect4.jsx
@@ -10,6 +10,8 @@ const AbtSect4 = () => {
           <h1>
             1. OCCUPATIONAL HEALTH AND SAFETY MANAGEMENT SYSTEM
           </h1>
+          </div>
+          <div>
           <p>
             At POAADIT, we are committed to ensuring the health, safety, and welfare of our workers, subcontractors, clients, and visitors.
             In compliance with ISO 45001:2018, relevant regulatory requirements, and industry best practices, we are dedicated to providing a safe and healthy working environment throughout all of our construction sites, workshops, and offices.
@@ -20,6 +22,8 @@ const AbtSect4 = () => {
           <h1>
            2. QUALITY MANAGEMENT SYSTEM
           </h1>
+          </div>
+        <div
           <p>
             At POAADIT, we are committed to leveraging our capabilities in the built environment, consistently meeting client and regulatory requirements, while promoting a healthy and safe environment.
             We strive for continuous improvement in every aspect of our operations, from project planning and design to construction and handover.

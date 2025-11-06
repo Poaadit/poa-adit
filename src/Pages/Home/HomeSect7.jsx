baec1b70e89541bb0d0269d34e0de8fed5b87084
Poaadit <105600040+Poaadit@users.noreply.github.com>
2025-11-06 07:52:54 +0000

    Update HomeSect7.jsx

--- src/Pages/Home/HomeSect7.jsx
+++ src/Pages/Home/HomeSect7.jsx
@@ -1,21 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import {  Button1, ColumnContainer } from "../../Components/ResponsiveConts";
+import { Button1, ColumnContainer } from "../../Components/ResponsiveConts";
 import "./Home.scss";
+
 const HomeSect7 = () => {
   return (
     <div className="homesect7cont">
       <ColumnContainer id="sect7cont">
         <div className="homesect7">
-            <div>
+          {/* Main content + logo container */}
+          <div className="content-logo-row">
+            <div className="text-area">
               <h1>Your Trusted</h1>
               <h1 id="trust_red">Construction Partner</h1>
               <h4>We are committed to superior and quality results.</h4>
+              <Link to="/contact-us">
+                <Button1 id="contcbtn">Contact Us</Button1>
+              </Link>
             </div>
-            <Link to ="/contact-us">
-            <Button1 id="contcbtn">Contact Us</Button1>
 
-            </Link>
+            {/* Logos container */}
+            <div className="logo-section">
+              <img
+                src="../../Assets/images/ISO9001.jpeg.jpg" 
+                alt="Logo 1"
+                className="partner-logo"
+              />
+              <img
+                src="../../Assets/images/45001 2018-01.jpg"
+                alt="Logo 2"
+                className="partner-logo"
+              />
+            </div>
+          </div>
         </div>
       </ColumnContainer>
     </div>
@@ -23,4 +40,3 @@ const HomeSect7 = () => {
 };
 
 export default HomeSect7;
-          
